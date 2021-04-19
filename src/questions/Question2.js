1c7aea65d7e56d6f91d432b1e3873c3f1c5b80e9
yuriymikhailidi <yumi4478@colorado.edu>
2021-04-19 15:05:08 -0600

    Fixed Q2

--- src/questions/Question2.js
+++ src/questions/Question2.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 
+/* Resources */
+/*
+  https://levelup.gitconnected.com/building-a-simple-dynamic-search-bar-in-react-js-f1659d64dfae
+*/
 export default function Question2 (props) {
   // Situation: Create a search bar that filters items in the list as the user types.
   // Feel free to refactor as you feel necessary.
 
   const [searchText, setSearchText] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   const shoppingList = [
     'Peanut Butter',
@@ -16,20 +22,29 @@ export default function Question2 (props) {
     'Cheese'
   ];
 
-  const handleSearchTextChange = () => {
+  const handleSearchTextChange = e => {
+    setSearchText(e.target.value);
+  };
 
-  }
+  useEffect(() => {
+    const res = shoppingList.filter(item => 
+      item.toLowerCase().includes(searchText)
+    );
+      setSearchResults(res);
+  }, [searchText])
 
   return (
-    <div>
-      <input value={searchText} onChange={handleSearchTextChange}/>
-      {shoppingList.map(item => {
-        return (
+    <div className="App">
+      <input
+      type="text"
+      value={searchText} 
+      onChange={handleSearchTextChange}
+      />
+      {searchResults.map(item => (
           <div>
             {item}
           </div>
-        )
-      })}
+      ))}
     </div>
   )
 }
